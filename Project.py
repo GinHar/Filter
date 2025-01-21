3d2814b42dd627fdd05ca2aef73ddbd8132cf4cd
GinHar <gihape20@gmail.com>
2025-01-21 17:57:42 +0100

    Update Project.py

--- Project.py
+++ Project.py
@@ -3,6 +3,7 @@ from Extract import extract_intensities,create_image_from_intensity
 from numpy import where, any, empty, array, arange, hstack,exp, polyfit, sqrt, std, sum, mean,diag, linspace,log, savetxt
 from pylab import show, imshow, plot, scatter, xlabel, ylabel, legend, title, errorbar
 import cv2
+import os
 
 
 # Video's path
@@ -11,6 +12,10 @@ frame_number = 0
 
 # Load the video with OpenCV
 cap = cv2.VideoCapture(video_path)
+
+#Create the folder
+folder = "Frames"
+os.makedirs(folder, exist_ok=True)
 
 if not cap.isOpened():
     print("Error.")
@@ -26,7 +31,12 @@ while True:
     
     frame_name = f"Frame"+str(frame_number)+".jpg"
     frame_number+=1
-    cv2.imwrite(frame_name, frame)
+    
+    # Full path to save the frame
+    save_path = os.path.join(folder, frame_name)
+    
+    #Save the frame
+    cv2.imwrite(save_path, frame)
     print(f"Frame save as: {frame_name}")
 
 
@@ -59,8 +69,13 @@ out = cv2.VideoWriter(video_name, fourcc, fps, (width, height))
 img = cv2.imread('Photo'+str(0)+'.jpg')
 out.write(img)  # Write the image in the video
 
+#Create the folder
+folder = "Photos"
+os.makedirs(folder, exist_ok=True)
+
+
 for i in range(frame_number,cap.get(cv2.CAP_PROP_FRAME_COUNT)):
-    image_path = "Frame"+str(i)+".jpg"  # Image's path
+    img_path = os.path.join(folder, 'Photo' + str(i) + '.jpg')
     I = extract_intensities(image_path)
     
     #Gauss+Sobel
@@ -70,9 +85,9 @@ for i in range(frame_number,cap.get(cv2.CAP_PROP_FRAME_COUNT)):
     #Threshold
     I = where(I >= 110, 255, 0)
     
-    
+    #Create the image, save the image and make the video
     img1 = create_image_from_intensity(I)
-    img1.save('Photo'+str(i)+'.jpg')
-    img = cv2.imread('Photo'+str(i)+'.jpg')
+    img1.save(img_path)
+    img = cv2.imread(img_path)
     out.write(img)  # Write the image in the video
     
